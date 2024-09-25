4aee98db8a940e0b3c8521f7eaaf855b4a287e54
Velasco <quarantine.sleep480@passinbox.com>
2024-09-26 00:08:05 +0200

    a

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,16 +7,29 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("GKFFD");
+        arrayList.add("TNANA");
+        arrayList.add("MPMSL");
+        arrayList.add("PSWME");
+        arrayList.add("LZMLF");
+        arrayList.add("JYEBV");
+        arrayList.add("YELNT");
+        arrayList.add("JSNKR");
+        arrayList.add("JFESF");
+        arrayList.add("TMJLL");
+        return arrayList;
+    }
 
+    public static boolean addElementToList(List<String> arrayList, String element) {
+        return arrayList.add(element); // Always returns true as ArrayList allows duplicates
     }
 
     public static void printElementsAndIndex(List<String> customList) {
 
     }
 
-    public static boolean addElementToList(List<String> customList, String element) {
 
-    }
 
     public static void main(String[] args) {
 
